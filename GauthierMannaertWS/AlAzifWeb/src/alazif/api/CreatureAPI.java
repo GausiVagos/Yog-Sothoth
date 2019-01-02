39272a47a73ad43b4493aa7af0b2ef93201e8915
Antoine@LAPTOP-HR0CP521.(none) <antoine.mannaert@gmail.com>
2019-01-02 17:20:00 +0100

    creatureAPI add

--- GauthierMannaertWS/AlAzifWeb/src/alazif/api/CreatureAPI.java
+++ GauthierMannaertWS/AlAzifWeb/src/alazif/api/CreatureAPI.java
@@ -1,5 +1,9 @@
 package alazif.api;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
 import java.util.HashSet;
 
 import javax.ws.rs.Consumes;
@@ -21,6 +25,9 @@ import alazif.pojos.Writer;
 
 @Path("creature")
 public class CreatureAPI {
+	
+	Connection conn = ProjectConnection.getInstance();
+	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response no()
@@ -56,8 +63,24 @@ public class CreatureAPI {
 	public Response add(Creature c)
 	{
 		//On l'ajoute ds la db
-		int id=999;
+		int id;
+		CallableStatement addcre = null;
+		try {
+			addcre = conn.prepareCall("? = call AddCreature(?, ?)");
+			
+			addcre.registerOutParameter(1,  Types.INTEGER);
+			addcre.setString(2, c.getDescription());
+			addcre.setInt(3, c.getCreatureId());
+			
+			addcre.executeUpdate();
+			id = addcre.getInt(1);
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			return Response.status(Status.NOT_ACCEPTABLE).build();
+		}
 		//On renvoie l'id nouvellement créé pour l'injecter dans l'objet
+		
 		return Response.status(Status.CREATED).entity("{\"id\":"+id+"}").build();
 	}
 	
