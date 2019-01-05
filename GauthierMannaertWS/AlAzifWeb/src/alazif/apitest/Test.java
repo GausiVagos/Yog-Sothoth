12ed4feedc8b1e63c90773633e693805d892ff62
Antoine@LAPTOP-HR0CP521.(none) <antoine.mannaert@gmail.com>
2019-01-05 16:16:15 +0100

    find by id fonctionnel

--- GauthierMannaertWS/AlAzifWeb/src/alazif/apitest/Test.java
+++ GauthierMannaertWS/AlAzifWeb/src/alazif/apitest/Test.java
@@ -1,17 +1,12 @@
 package alazif.apitest;
 
-import java.util.HashSet;
-import java.util.Set;
-
+import alazif.api.CreatureAPI;
 import alazif.pojos.Creature;
-import alazif.pojos.CreatureName;
-import alazif.pojos.Novel;
-import alazif.pojos.Writer;
 
 public class Test {
 
 	public static void main(String[] args) 
-	{
+	{/*
 		Set<Novel> novels = new HashSet<Novel>();
 		novels.add(new Novel(1, null, 0, null, null, null));
 		novels.add(new Novel(2, null, 0, null, null, null));
@@ -20,6 +15,10 @@ public class Test {
 		names.add(new CreatureName("Bobby l'invincible"));
 		Creature bobby = new Creature("Salut, moi c'est Bobby.", new Writer(1,"", null, null), novels, names);
 		//System.out.println(cdao.create(bobby));
+		 */
+		Creature c = new Creature();
+		CreatureAPI capi = new CreatureAPI();
+		capi.getById(5);
 	}
 
 }
